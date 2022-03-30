5849749e536f24dacc6cd83fe3461c9bf3e36c00
LeAnhXuan01 <2051063612@e.tlu.edu.vn>
2022-03-30 21:59:49 +0700

    edit js

--- js/home.js
+++ js/home.js
@@ -17,7 +17,7 @@ window.addEventListener('scroll', () =>{
 collagImages.map((item, i) => {
     item.addEventListener('mouseover', () => {
         collagImages.map((image, index) => {
-            if(index != 1){
+            if(index != i){
                 image.style.filter = 'blur(10px)';
                 item.style.zIndex = 2
             }
